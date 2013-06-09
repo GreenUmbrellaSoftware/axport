c4663e8d6599e7646d0e2d84fd1b542d79c3a424
Kris Gholson <kris@greenumbrellasoftware.com>
2013-06-09 10:53:16 -0400

    remove unused stuff, add a default password

--- src/main/java/com/greenumbrellasoftware/axport/Exporter.java
+++ src/main/java/com/greenumbrellasoftware/axport/Exporter.java
@@ -1,13 +1,7 @@
 package com.greenumbrellasoftware.axport;
 
-import com.healthmarketscience.jackcess.*;
-import org.apache.commons.beanutils.DynaBean;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.apache.ddlutils.Platform;
-import org.apache.ddlutils.PlatformFactory;
+import static org.apache.commons.lang.StringUtils.remove;
 
-import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -16,196 +10,263 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import static org.apache.commons.lang.StringUtils.remove;
+import javax.sql.DataSource;
+
+import org.apache.commons.beanutils.DynaBean;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.ddlutils.Platform;
+import org.apache.ddlutils.PlatformFactory;
+
+import com.healthmarketscience.jackcess.Column;
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.Index;
+import com.healthmarketscience.jackcess.IndexData;
+import com.healthmarketscience.jackcess.Table;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Kris Gholson
- * Date: 3/16/13
- * Time: 1:56 PM
+ * Export data from a Team Manager .mdb file to a MySQL database.
  */
 public class Exporter {
 
-    private static final Log LOG = LogFactory.getLog(Exporter.class);
-    private static final String[] MYSQL_RESERVED_WORDS = {"ACCESSIBLE", "ALGORITHM", "ANALYZE", "ASENSITIVE", "BEFORE", "BIGINT", "BINARY", "BLOB", "CALL", "CHANGE", "CONDITION", "COPY", "DATA", "DATABASE", "DATABASES", "DAY_HOUR", "DAY_MICROSECOND", "DAY_MINUTE", "DAY_SECOND", "DELAYED", "DESC", "DETERMINISTIC", "DIRECTORY", "DISCARD", "DISTINCTROW", "DIV", "DUAL", "EACH", "ELSEIF", "ENCLOSED", "ESCAPED", "EXCHANGE", "EXCLUSIVE", "EXIT", "EXPLAIN", "EXPORT", "FLOAT4", "FLOAT8", "FLUSH", "FORCE", "FULLTEXT", "GENERAL", "GROUP", "HIGH_PRIORITY", "HOUR_MICROSECOND", "HOUR_MINUTE", "HOUR_SECOND", "IF", "IGNORE", "IGNORE_SERVER_IDS", "IMPORT", "INFILE", "INOUT", "INPLACE", "INT1", "INT2", "INT3", "INT4", "INT8", "ITERATE", "KEYS", "KILL", "LEAVE", "LIMIT", "LINEAR", "LINES", "LOAD", "LOCALTIME", "LOCALTIMESTAMP", "LOCK", "LONG", "LONGBLOB", "LONGTEXT", "LOOP", "LOW_PRIORITY", "MASTER_HEARTBEAT_PERIOD", "MAXVALUE", "MEDIUMBLOB", "MEDIUMINT", "MEDIUMTEXT", "MIDDLEINT", "MINUTE_MICROSECOND", "MINUTE_SECOND", "MOD", "MODIFIES", "NO_WRITE_TO_BINLOG", "OPTIMIZE", "OPTIONALLY", "OUT", "OUTFILE", "PARTITION", "PRIMARY", "PURGE", "RANGE", "READS", "READ_ONLY", "READ_WRITE", "REBUILD", "REGEXP", "RELEASE", "REMOVE", "RENAME", "REORGANIZE", "REPAIR", "REPEAT", "REPLACE", "REQUIRE", "RESIGNAL", "RETURN", "RLIKE", "SCHEMAS", "SECOND_MICROSECOND", "SENSITIVE", "SEPARATOR", "SHARED", "SHOW", "SIGNAL", "SLOW", "SPATIAL", "SPECIFIC", "SQLEXCEPTION", "SQL_BIG_RESULT", "SQL_CALC_FOUND_ROWS", "SQL_SMALL_RESULT", "SSL", "STARTING", "STRAIGHT_JOIN", "TABLES", "TABLESPACE", "TERMINATED", "TINYBLOB", "TINYINT", "TINYTEXT", "TRIGGER", "UNDO", "UNLOCK", "UNSIGNED", "USE", "UTC_DATE", "UTC_TIME", "UTC_TIMESTAMP", "VARBINARY", "VARCHARACTER", "WHILE", "X509", "XOR", "YEAR_MONTH", "ZEROFILL"};
+	private static final Log LOG = LogFactory.getLog(Exporter.class);
+	private static final String[] MYSQL_RESERVED_WORDS = { "ACCESSIBLE",
+			"ALGORITHM", "ANALYZE", "ASENSITIVE", "BEFORE", "BIGINT", "BINARY",
+			"BLOB", "CALL", "CHANGE", "CONDITION", "COPY", "DATA", "DATABASE",
+			"DATABASES", "DAY_HOUR", "DAY_MICROSECOND", "DAY_MINUTE",
+			"DAY_SECOND", "DELAYED", "DESC", "DETERMINISTIC", "DIRECTORY",
+			"DISCARD", "DISTINCTROW", "DIV", "DUAL", "EACH", "ELSEIF",
+			"ENCLOSED", "ESCAPED", "EXCHANGE", "EXCLUSIVE", "EXIT", "EXPLAIN",
+			"EXPORT", "FLOAT4", "FLOAT8", "FLUSH", "FORCE", "FULLTEXT",
+			"GENERAL", "GROUP", "HIGH_PRIORITY", "HOUR_MICROSECOND",
+			"HOUR_MINUTE", "HOUR_SECOND", "IF", "IGNORE", "IGNORE_SERVER_IDS",
+			"IMPORT", "INFILE", "INOUT", "INPLACE", "INT1", "INT2", "INT3",
+			"INT4", "INT8", "ITERATE", "KEYS", "KILL", "LEAVE", "LIMIT",
+			"LINEAR", "LINES", "LOAD", "LOCALTIME", "LOCALTIMESTAMP", "LOCK",
+			"LONG", "LONGBLOB", "LONGTEXT", "LOOP", "LOW_PRIORITY",
+			"MASTER_HEARTBEAT_PERIOD", "MAXVALUE", "MEDIUMBLOB", "MEDIUMINT",
+			"MEDIUMTEXT", "MIDDLEINT", "MINUTE_MICROSECOND", "MINUTE_SECOND",
+			"MOD", "MODIFIES", "NO_WRITE_TO_BINLOG", "OPTIMIZE", "OPTIONALLY",
+			"OUT", "OUTFILE", "PARTITION", "PRIMARY", "PURGE", "RANGE",
+			"READS", "READ_ONLY", "READ_WRITE", "REBUILD", "REGEXP", "RELEASE",
+			"REMOVE", "RENAME", "REORGANIZE", "REPAIR", "REPEAT", "REPLACE",
+			"REQUIRE", "RESIGNAL", "RETURN", "RLIKE", "SCHEMAS",
+			"SECOND_MICROSECOND", "SENSITIVE", "SEPARATOR", "SHARED", "SHOW",
+			"SIGNAL", "SLOW", "SPATIAL", "SPECIFIC", "SQLEXCEPTION",
+			"SQL_BIG_RESULT", "SQL_CALC_FOUND_ROWS", "SQL_SMALL_RESULT", "SSL",
+			"STARTING", "STRAIGHT_JOIN", "TABLES", "TABLESPACE", "TERMINATED",
+			"TINYBLOB", "TINYINT", "TINYTEXT", "TRIGGER", "UNDO", "UNLOCK",
+			"UNSIGNED", "USE", "UTC_DATE", "UTC_TIME", "UTC_TIMESTAMP",
+			"VARBINARY", "VARCHARACTER", "WHILE", "X509", "XOR", "YEAR_MONTH",
+			"ZEROFILL" };
 
-    private static String cleanName(String originalName) {
+	private static String cleanName(String originalName) {
 
-        String newName = originalName;
-        if (Arrays.asList(MYSQL_RESERVED_WORDS).contains(newName.toUpperCase())) {
-            newName = String.format("_%s", newName);
-        }
-        newName = remove(newName, "(");
-        newName = remove(newName, ")");
-        newName = remove(newName, ".");
+		String newName = originalName;
+		if (Arrays.asList(MYSQL_RESERVED_WORDS).contains(newName.toUpperCase())) {
+			newName = String.format("_%s", newName);
+		}
+		newName = remove(newName, "(");
+		newName = remove(newName, ")");
+		newName = remove(newName, ".");
 
-        return newName;
-    }
+		return newName;
+	}
 
-    public static org.apache.ddlutils.model.Database exportSchema(final String databaseName, final File mdbFile) throws IOException, SQLException {
+	public static org.apache.ddlutils.model.Database exportSchema(
+			final String databaseName, final File mdbFile) throws IOException,
+			SQLException {
 
-        LOG.info(String.format("Exporting schema from '%s'", mdbFile.getAbsolutePath()));
+		LOG.info(String.format("Exporting schema from '%s'",
+				mdbFile.getAbsolutePath()));
 
-        org.apache.ddlutils.model.Database database = new org.apache.ddlutils.model.Database();
+		org.apache.ddlutils.model.Database database = new org.apache.ddlutils.model.Database();
 
-        Database accessDb = Database.open(mdbFile, true);
+		Database accessDb = Database.open(mdbFile, true);
 
-        for (String tableName : accessDb.getTableNames()) {
+		for (String tableName : accessDb.getTableNames()) {
 
-            Table accessTable = accessDb.getTable(tableName);
+			Table accessTable = accessDb.getTable(tableName);
 
-            org.apache.ddlutils.model.Table table = new org.apache.ddlutils.model.Table();
-            table.setName(cleanName(accessTable.getName()));
+			org.apache.ddlutils.model.Table table = new org.apache.ddlutils.model.Table();
+			table.setName(cleanName(accessTable.getName()));
 
-            if (LOG.isTraceEnabled()) {
-                LOG.trace(String.format("TABLE: %s", table.getName()));
-            }
+			if (LOG.isTraceEnabled()) {
+				LOG.trace(String.format("TABLE: %s", table.getName()));
+			}
 
-            // Create all columns
-            for (Column accessColumn : accessTable.getColumns()) {
+			// Create all columns
+			for (Column accessColumn : accessTable.getColumns()) {
 
-                org.apache.ddlutils.model.Column column = new org.apache.ddlutils.model.Column();
-                column.setName(cleanName(accessColumn.getName()));
-                column.setTypeCode(accessColumn.getSQLType());
-                column.setSizeAndScale(accessColumn.getLength(), accessColumn.getScale());
+				org.apache.ddlutils.model.Column column = new org.apache.ddlutils.model.Column();
+				column.setName(cleanName(accessColumn.getName()));
+				column.setTypeCode(accessColumn.getSQLType());
+				column.setSizeAndScale(accessColumn.getLength(),
+						accessColumn.getScale());
 
-                if (LOG.isTraceEnabled()) {
-                    LOG.trace(String.format("COLUMN: %s  type: %s, typeCode: %s, size: %s, scale: %s", column.getName(), column.getType(), column.getTypeCode(), column.getSize(), column.getScale()));
-                }
-                table.addColumn(column);
-            }
+				if (LOG.isTraceEnabled()) {
+					LOG.trace(String
+							.format("COLUMN: %s  type: %s, typeCode: %s, size: %s, scale: %s",
+									column.getName(), column.getType(),
+									column.getTypeCode(), column.getSize(),
+									column.getScale()));
+				}
+				table.addColumn(column);
+			}
 
-            // Loop though the indexes (unique and non-unique)
-            for (Index accessIndex : accessTable.getIndexes()) {
+			// Loop though the indexes (unique and non-unique)
+			for (Index accessIndex : accessTable.getIndexes()) {
 
-                org.apache.ddlutils.model.Index index = accessIndex.isUnique() ? new org.apache.ddlutils.model.UniqueIndex() : new org.apache.ddlutils.model.NonUniqueIndex();
-                index.setName(cleanName(accessIndex.getName()));
+				org.apache.ddlutils.model.Index index = accessIndex.isUnique() ? new org.apache.ddlutils.model.UniqueIndex()
+						: new org.apache.ddlutils.model.NonUniqueIndex();
+				index.setName(cleanName(accessIndex.getName()));
 
-                if (LOG.isTraceEnabled()) {
-                    LOG.trace(String.format("INDEX: %s", index.getName()));
-                }
+				if (LOG.isTraceEnabled()) {
+					LOG.trace(String.format("INDEX: %s", index.getName()));
+				}
 
-                for (IndexData.ColumnDescriptor accessColumnDescriptor : accessIndex.getColumns()) {
-                    Column accessColumn = accessColumnDescriptor.getColumn();
-                    org.apache.ddlutils.model.Column column = table.findColumn(cleanName(accessColumn.getName()));
-                    column.setPrimaryKey(accessIndex.isPrimaryKey());
-                    org.apache.ddlutils.model.IndexColumn indexColumn = new org.apache.ddlutils.model.IndexColumn(column);
-                    index.addColumn(indexColumn);
-                    if (LOG.isTraceEnabled()) {
-                        LOG.trace(String.format("INDEX COLUMN: %s  isPrimary: %s", indexColumn.getName(), column.isPrimaryKey()));
-                    }
+				for (IndexData.ColumnDescriptor accessColumnDescriptor : accessIndex
+						.getColumns()) {
+					Column accessColumn = accessColumnDescriptor.getColumn();
+					org.apache.ddlutils.model.Column column = table
+							.findColumn(cleanName(accessColumn.getName()));
+					column.setPrimaryKey(accessIndex.isPrimaryKey());
+					org.apache.ddlutils.model.IndexColumn indexColumn = new org.apache.ddlutils.model.IndexColumn(
+							column);
+					index.addColumn(indexColumn);
+					if (LOG.isTraceEnabled()) {
+						LOG.trace(String.format(
+								"INDEX COLUMN: %s  isPrimary: %s",
+								indexColumn.getName(), column.isPrimaryKey()));
+					}
 
-                }
+				}
 
-                table.addIndex(index);
-            }
+				table.addIndex(index);
+			}
 
-            database.addTable(table);
-        }
-        accessDb.close();
-        database.setName(databaseName);
-        return database;
-    }
+			database.addTable(table);
+		}
+		accessDb.close();
+		database.setName(databaseName);
+		return database;
+	}
 
-    /**
-     * Read the schema and data from the given Access database file and create the corresponding schema and data
-     * in the passed in data source.  The data source can be from any database supported by the Apache DdlUtils project.
-     *
-     * @param dataSource
-     * @param mdbFile
-     * @throws IOException
-     * @throws SQLException
-     */
-    public static void createDatabaseFromAccessFile(final DataSource dataSource, final File mdbFile) throws IOException, SQLException {
+	/**
+	 * Read the schema and data from the given Access database file and create
+	 * the corresponding schema and data in the passed in data source. The data
+	 * source can be from any database supported by the Apache DdlUtils project.
+	 * 
+	 * @param dataSource
+	 * @param mdbFile
+	 * @throws IOException
+	 * @throws SQLException
+	 */
+	public static void createDatabaseFromAccessFile(
+			final DataSource dataSource, final File mdbFile)
+			throws IOException, SQLException {
 
-        Platform platform = PlatformFactory.createNewPlatformInstance(dataSource);
-        org.apache.ddlutils.model.Database database = exportSchema(mdbFile.getName(), mdbFile);
-        platform.createTables(database, false, false);
+		Platform platform = PlatformFactory
+				.createNewPlatformInstance(dataSource);
+		org.apache.ddlutils.model.Database database = exportSchema(
+				mdbFile.getName(), mdbFile);
+		platform.createTables(database, false, false);
 
-        Database accessDb = Database.open(mdbFile, true);
+		Database accessDb = Database.open(mdbFile, true);
 
-        List<DynaBean> records = new ArrayList<DynaBean>();
-        for (org.apache.ddlutils.model.Table dbTable : database.getTables()) {
+		List<DynaBean> records = new ArrayList<DynaBean>();
+		for (org.apache.ddlutils.model.Table dbTable : database.getTables()) {
 
-            Table table = accessDb.getTable(dbTable.getName());
+			Table table = accessDb.getTable(dbTable.getName());
 
-            List<Column> columns = table.getColumns();
+			List<Column> columns = table.getColumns();
 
-            for (Map<String, Object> row : table) {
+			for (Map<String, Object> row : table) {
 
-                DynaBean record = database.createDynaBeanFor(dbTable.getName(), false);
+				DynaBean record = database.createDynaBeanFor(dbTable.getName(),
+						false);
 
-                for (Column col : columns) {
-                    Object val = row.get(col.getName());
-                    if (val != null) {
-                        String columnName = cleanName(col.getName());
-                        record.set(columnName, val);
-                    }
-                } // end iterating through columns
+				for (Column col : columns) {
+					Object val = row.get(col.getName());
+					if (val != null) {
+						String columnName = cleanName(col.getName());
+						record.set(columnName, val);
+					}
+				} // end iterating through columns
 
-                records.add(record);
-            } // end iterating through rows
-        } // end iterating through table names
+				records.add(record);
+			} // end iterating through rows
+		} // end iterating through table names
 
-        platform.insert(database, records);
-    }
+		platform.insert(database, records);
+	}
 
-    /**
-     * Read the schema and data from the given Access database file and create the ddl for the appropriate platform for
-     * the given dataSource.  The data source can be from any database supported by the Apache DdlUtils project.
-     *
-     * @param dataSource
-     * @param mdbFile
-     * @throws IOException
-     * @throws SQLException
-     */
-    public static String createDatabaseDdlFromAccessFile(final DataSource dataSource, final File mdbFile) throws IOException, SQLException {
+	/**
+	 * Read the schema and data from the given Access database file and create
+	 * the ddl for the appropriate platform for the given dataSource. The data
+	 * source can be from any database supported by the Apache DdlUtils project.
+	 * 
+	 * @param dataSource
+	 * @param mdbFile
+	 * @throws IOException
+	 * @throws SQLException
+	 */
+	public static String createDatabaseDdlFromAccessFile(
+			final DataSource dataSource, final File mdbFile)
+			throws IOException, SQLException {
 
-        Platform platform = PlatformFactory.createNewPlatformInstance(dataSource);
-        org.apache.ddlutils.model.Database database = exportSchema(mdbFile.getName(), mdbFile);
-        return platform.getCreateTablesSql(database, true, false);
-    }
+		Platform platform = PlatformFactory
+				.createNewPlatformInstance(dataSource);
+		org.apache.ddlutils.model.Database database = exportSchema(
+				mdbFile.getName(), mdbFile);
+		return platform.getCreateTablesSql(database, true, false);
+	}
 
-    /**
-     * Read the schema and data from the given Access database file and create the dml to inser the data
-     * for all records in the database.
-     * The data source can be from any database supported by the Apache DdlUtils project.
-     *
-     * @param dataSource
-     * @param mdbFile
-     * @throws IOException
-     * @throws SQLException
-     */
-    public static String createDatabaseInsertStatementsFromAccessFile(final DataSource dataSource, final File mdbFile) throws IOException, SQLException {
+	/**
+	 * Read the schema and data from the given Access database file and create
+	 * the dml to inser the data for all records in the database. The data
+	 * source can be from any database supported by the Apache DdlUtils project.
+	 * 
+	 * @param dataSource
+	 * @param mdbFile
+	 * @throws IOException
+	 * @throws SQLException
+	 */
+	public static String createDatabaseInsertStatementsFromAccessFile(
+			final DataSource dataSource, final File mdbFile)
+			throws IOException, SQLException {
 
-        StringBuilder sb = new StringBuilder();
-        Platform platform = PlatformFactory.createNewPlatformInstance(dataSource);
-        org.apache.ddlutils.model.Database database = exportSchema(mdbFile.getName(), mdbFile);
+		StringBuilder sb = new StringBuilder();
+		Platform platform = PlatformFactory
+				.createNewPlatformInstance(dataSource);
+		org.apache.ddlutils.model.Database database = exportSchema(
+				mdbFile.getName(), mdbFile);
 
-        Database accessDb = Database.open(mdbFile, true);
+		Database accessDb = Database.open(mdbFile, true);
 
-        for (org.apache.ddlutils.model.Table dbTable : database.getTables()) {
+		for (org.apache.ddlutils.model.Table dbTable : database.getTables()) {
 
-            Table table = accessDb.getTable(dbTable.getName());
+			Table table = accessDb.getTable(dbTable.getName());
 
-            List<Column> columns = table.getColumns();
+			List<Column> columns = table.getColumns();
 
-            for (Map<String, Object> row : table) {
+			for (Map<String, Object> row : table) {
 
-                DynaBean record = database.createDynaBeanFor(dbTable.getName(), false);
+				DynaBean record = database.createDynaBeanFor(dbTable.getName(),
+						false);
 
-                for (Column col : columns) {
-                    Object val = row.get(col.getName());
-                    if (val != null) {
-                        String columnName = cleanName(col.getName());
-                        record.set(columnName, val);
-                    }
-                } // end iterating through columns
+				for (Column col : columns) {
+					Object val = row.get(col.getName());
+					if (val != null) {
+						String columnName = cleanName(col.getName());
+						record.set(columnName, val);
+					}
+				} // end iterating through columns
 
-                sb.append(platform.getInsertSql(database, record) + "\n");
-            } // end iterating through rows
-        } // end iterating through table names
-        return sb.toString();
-    }
+				sb.append(platform.getInsertSql(database, record) + "\n");
+			} // end iterating through rows
+		} // end iterating through table names
+		return sb.toString();
+	}
 
 }
--- src/test/java/com/greenumbrellasoftware/axport/ExporterTest.java
+++ src/test/java/com/greenumbrellasoftware/axport/ExporterTest.java
@@ -1,74 +1,80 @@
 package com.greenumbrellasoftware.axport;
 
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.io.File;
 
-import org.apache.ddlutils.model.Database;
 import org.junit.Test;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-//import com.google.cloud.sql.jdbc.GoogleDataSource;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Kris
- * Date: 3/16/13
- * Time: 2:28 PM
- * To change this template use File | Settings | File Templates.
+ * Test to exercise the static methods in the Exporter class. The most important
+ * test is testCreateDatabaseFromAccessFile which will create the complete mysql
+ * data structure and data in a mysql database (running at the default settings
+ * and with the configured test username and password).
+ * 
  */
 public class ExporterTest {
 
-    private static final String DB = "rsta12";
-    private static final String DB_USER = "root";
-//    private static final String DB_PASSWORD = "password";
+	private static final String YEAR = "12";
 
-    @Test
-    public void testCreateDatabaseFromAccessFile() throws Exception {
-        MysqlDataSource ds = new MysqlDataSource();
-        ds.setDatabaseName(DB);
-        ds.setUser(DB_USER);
-//        ds.setPassword(DB_PASSWORD);
+	private static final String DB = "rsta" + YEAR;
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "password";
+	private static final String TEAM_MANAGER_FILENAME = "src/test/resources/RSTA_20"
+			+ YEAR + "_League.mdb";
 
-        Exporter.createDatabaseFromAccessFile(ds, new File("src/test/resources/RSTA_2012_League.mdb"));
+	@Test
+	public void testCreateDatabaseFromAccessFile() throws Exception {
+		MysqlDataSource ds = new MysqlDataSource();
+		ds.setDatabaseName(DB);
+		ds.setUser(DB_USER);
+		ds.setPassword(DB_PASSWORD);
 
-    }
+		Exporter.createDatabaseFromAccessFile(ds, new File(
+				TEAM_MANAGER_FILENAME));
 
-    @Test
-    public void testCreateDatabaseSql() throws Exception {
-        MysqlDataSource ds = new MysqlDataSource();
-        ds.setDatabaseName(DB);
-        ds.setUser(DB_USER);
+	}
 
-        String createSql = Exporter.createDatabaseDdlFromAccessFile(ds, new File("src/test/resources/RSTA_2012_League.mdb") ) ;
-        Path createSqlPath = Paths.get("../swimreston/src/main/resources/createDatabase.sql");
-        Files.deleteIfExists(createSqlPath);
-        createSqlPath = Files.createFile(createSqlPath) ;
-        BufferedWriter writer = Files.newBufferedWriter(createSqlPath, Charset.forName("UTF-8"));
-        writer.append(createSql);
-        writer.flush();
-
-    }
-
-    @Test
-    public void testCreateDatabaseInsertStatementsFromAccessFile() throws Exception {
-        MysqlDataSource ds = new MysqlDataSource();
-        ds.setDatabaseName(DB);
-        ds.setUser(DB_USER);
-
-        String insertSql  = Exporter.createDatabaseInsertStatementsFromAccessFile(ds, new File("src/test/resources/RSTA_2012_League.mdb") ) ;
-        Path insertSqlPath = Paths.get("../swimreston/src/main/resources/insertData.sql");
-        Files.deleteIfExists(insertSqlPath);
-        insertSqlPath = Files.createFile(insertSqlPath) ;
-        BufferedWriter writer = Files.newBufferedWriter(insertSqlPath, Charset.forName("UTF-8"));
-        writer.append(insertSql);
-        writer.flush();
-
-    }
-
+	// @Test
+	// public void testCreateDatabaseSql() throws Exception {
+	// MysqlDataSource ds = new MysqlDataSource();
+	// ds.setDatabaseName(DB);
+	// ds.setUser(DB_USER);
+	// ds.setPassword(DB_PASSWORD);
+	//
+	// String createSql = Exporter.createDatabaseDdlFromAccessFile(ds,
+	// new File(TEAM_MANAGER_FILENAME));
+	// Path createSqlPath = Paths
+	// .get("../swimreston/src/main/resources/createDatabase.sql");
+	// Files.deleteIfExists(createSqlPath);
+	// createSqlPath = Files.createFile(createSqlPath);
+	// BufferedWriter writer = Files.newBufferedWriter(createSqlPath,
+	// Charset.forName("UTF-8"));
+	// writer.append(createSql);
+	// writer.flush();
+	//
+	// }
+	//
+	// @Test
+	// public void testCreateDatabaseInsertStatementsFromAccessFile()
+	// throws Exception {
+	// MysqlDataSource ds = new MysqlDataSource();
+	// ds.setDatabaseName(DB);
+	// ds.setUser(DB_USER);
+	// ds.setPassword(DB_PASSWORD);
+	//
+	// String insertSql = Exporter
+	// .createDatabaseInsertStatementsFromAccessFile(ds, new File(
+	// TEAM_MANAGER_FILENAME));
+	// Path insertSqlPath = Paths
+	// .get("../swimreston/src/main/resources/insertData.sql");
+	// Files.deleteIfExists(insertSqlPath);
+	// insertSqlPath = Files.createFile(insertSqlPath);
+	// BufferedWriter writer = Files.newBufferedWriter(insertSqlPath,
+	// Charset.forName("UTF-8"));
+	// writer.append(insertSql);
+	// writer.flush();
+	//
+	// }
 
 }
